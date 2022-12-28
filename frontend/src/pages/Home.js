81aa0298610c4fc4d2fb10959876ad9931530621
Rami B <ramibasheer9@gmail.com>
2022-12-28 23:24:45 +0100

    applying css

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react";
+import WorkoutDetails from "../components/WorkoutDetails";
+
 const Home = () => {
-    return (
-        <div className="home">
-        <h1>Home</h1>
-        </div>
-    );
-    }
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const res = await fetch("/api/workouts");
+      const json = await res.json();
+      if (res.ok) {
+        setWorkouts(json);
+      }
+    };
+    fetchWorkouts();
+  }, []);
+
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) =>( <WorkoutDetails key={workout._id} workout={workout} />))}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
